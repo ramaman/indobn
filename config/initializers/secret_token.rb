c1e4562ea061ebf44c80c297f9ebb232d15128d1
ramaman <rjgm02@gmail.com>
2013-05-23 01:05:00 +0200

    added files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Indobn::Application.config.secret_token = 'b49edffc9cd65cc08a6cc3b6ae3656695c9d85c9b73f57a962fe3a1c3e906155bf7255b8f78aaf1734caa41abc5cdd05e39a3554904fa7fdcc8f2dc0a6ad69d3'
